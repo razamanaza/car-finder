0e19ebb6ed5a83c0f1d27f2cbb1d766459130133
Aleksandr Gordeev <razamanaza@gmail.com>
2025-06-20 17:51:26 +1200

    Init with simple client

---
+++ client/src/layout/AppLayout.tsx
@@ -0,0 +1,5 @@
+const AppLayout = () => {
+  return <h1 className="text-3xl font-bold">Car tracker start app</h1>;
+};
+
+export default AppLayout;
---
+++ client/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
+dotenv.config({ path: "client.env" });
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  plugins: [react(), tailwindcss()],
+});
