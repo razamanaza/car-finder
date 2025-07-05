e3d510b589723f486081f5638f8aade848cedd3a
Aleksandr Gordeev <razamanaza@gmail.com>
2025-07-05 14:55:28 +1200

    Add shadcn

--- client/src/layout/AppLayout.tsx
+++ client/src/layout/AppLayout.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import { Outlet } from "@tanstack/react-router";
 
 const AppLayout = () => {
   return (
     <>
       <h1 className="text-3xl font-bold">Car tracker start app</h1>
+      <Button variant={"default"}>Button</Button>
       <Outlet />
     </>
   );
